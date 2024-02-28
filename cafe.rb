71c60254bed27c4f984aa82024597fea2abb150c
mariri <rmrmrm7rmrmrm@gmail.com>
2024-02-28 22:24:44 +0900

    プラクティス「プログラムの修正」課題を提出

--- cafe.rb
+++ cafe.rb
@@ -18,10 +18,16 @@ def take_order(menus)
   menus.each.with_index(1) do |menu, i|
     puts "(#{i})#{menu[:name]}: #{menu[:price]}円"
   end
+
   print '>'
-  order_number = gets.to_i
-  puts "#{menus[order_number][:name]}(#{menus[order_number][:price]}円)ですね。"
-  order_number
+  order_number = gets.chomp.to_i
+  if order_number.between?(1, menus.size)
+    puts "#{menus[order_number - 1][:name]}(#{menus[order_number - 1][:price]}円)ですね。"
+    return order_number - 1
+  else
+    puts "1〜#{menus.size}を選択してください"
+    return take_order(menus)
+  end
 end
 
 puts 'bugカフェへようこそ！ご注文は？ 番号でどうぞ'
@@ -30,5 +36,5 @@ order1 = take_order(DRINKS)
 puts 'フードメニューはいかがですか?'
 order2 = take_order(FOODS)
 
-total = FOODS[order1][:price] + DRINKS[order2][:price]
+total = FOODS[order2][:price].to_i + DRINKS[order1][:price].to_i
 puts "お会計は#{total}円になります。ありがとうございました！"
